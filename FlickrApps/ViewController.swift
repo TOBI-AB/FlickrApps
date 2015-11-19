38bd13931bd8cc4329bb6e450659fd9ed544735b
leversol <test_win8@outlook.com>
2015-11-19 11:45:31 +0000

    Setup UI Connect UI to ViewController

--- FlickrApps/ViewController.swift
+++ FlickrApps/ViewController.swift
@@ -8,7 +8,25 @@
 
 import UIKit
 
+let BASE_URL = "https://api.flickr.com/services/rest/"
+let METHOD_NAME = "flick.galleries.getPhotos"
+let API_KEY = "a1a1379b857780360341b2ac6a3c7d73"
+let GALLERY_ID = "5704-72157622566655097"
+let EXTRAS = "url_m"
+let DATA_FORMAT = "json"
+let NO_JSON_CALLBACK = "1"
+
 class ViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var phraseTextField: UITextField!
+    @IBOutlet weak var latitudeTextField: UITextField!
+    @IBOutlet weak var longtitudeTextField: UITextField!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +38,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func searchPhotoByPhrase(sender: UIButton) {
+    }
+    
+    @IBAction func searchPhotoByLatLon(sender: UIButton) {
+    }
+    
+    
+    
+    
 
 }
 
